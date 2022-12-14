ce1c0dca1fe5ff59e11f2acdb87f4c37290e90f8
Adeeon <cgt10dt@gmail.com>
2022-12-14 08:18:44 -0600

    Udemy 6-8

---
+++ Udemy/100 Days of Code Python/Day 6/Ex 6.py
@@ -0,0 +1,20 @@
+# Day 6 consisted of indentation, while loops, if statements and booleans using reeborg's world (reeborg.ca)
+# while completing hurdles 1, 2, 3, 4, the maze and edge cases for the maze that could trigger infinite loops
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+#
+# Above code works succesfully for hurdles 1, 2, 3, 4, and still gets caught on the edge cases.
